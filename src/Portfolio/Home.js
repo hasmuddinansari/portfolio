e3c3e92a3184319aed4a251e47333b1fea8f324f
Hasmuddin <mdaliansari33@gmail.com>
2020-02-24 18:07:18 +0530

    new font

--- src/Portfolio/Home.js
+++ src/Portfolio/Home.js
@@ -31,9 +31,15 @@ class Home extends Component {
             <div>
               <span className="header">Hello I'm</span>
 
-              <h1>Hasmuddin Ansari</h1>
+              <h1 id="poppin">Hasmuddin Ansari</h1>
 
-              <h3>Web Developer</h3>
+              <h3 className="text-success">Full Stack Developer</h3>
+              <p className="d-flex flex-wrap">
+              A dedicated and passionate full stack developer
+              trained and skilled in MERN stack at Masai school,
+              looking forward to join a startup where I can enhance 
+              my skills and can become an asset to the organisation.
+              </p>
 
               <p>
                 <span>
