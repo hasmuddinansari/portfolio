1db3e6bbbd8ebdfa8b7eda7086248616800a8428
Hasmuddin <mdaliansari33@gmail.com>
2020-02-25 14:36:32 +0530

    github bar

--- src/Portfolio/Githubs.js
+++ src/Portfolio/Githubs.js
@@ -6,7 +6,7 @@ import ReactTooltip from "react-tooltip"
 const Githubs = () => {
     const cm = [{num:"1000+", title:"Hours of Coding"}, {num:"200+", title:"Hackerrank Problems"}, {num:"350+", title:"Git Commits"},  {num:"12+", title:"Projects"}]
     return (
-        <div className="container">
+        <div className="container my-4">
             <div className="row justify-content-between" >
                 {cm.map(d=>{
                     return <div className="col-lg-3 row justify-content-center col-md-6 col-12">
@@ -16,8 +16,8 @@ const Githubs = () => {
                         <h4 className="my-2">{d.title}</h4>
                     </div>
                 })}
-            <div className="col-12 row justify-content-center">
-            <GitHubCalendar className="size-inc" username="hasmuddinansari" fullYear={true} color="blue">
+            <div className="col-12 row justify-content-center my-5">
+            <GitHubCalendar fontSize={25} blockMargin={3} blockSize={33} className="size-inc" username="hasmuddinansari" fullYear={true} color="blue">
             <ReactTooltip delayShow={50} html />
             </GitHubCalendar>
             </div>
