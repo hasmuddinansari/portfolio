dd7fb16ab100f088703cb06ca99cad98b5c51fe5
Hasmuddin <mdaliansari33@gmail.com>
2020-05-11 11:22:24 +0530

    chat app

--- src/Portfolio/ProjectData.js
+++ src/Portfolio/ProjectData.js
@@ -1,4 +1,12 @@
 export default [
+
+		{
+        	"img": "/images/chatroom.png", "website": "https://chatroomplay.herokuapp.com", "title": "Chat App", "desc":"User will allow to chat to any user with same room name whoever joined with same name", "github": "https://github.com/hasmuddinansari/chatApp", "tech": [
+            "/icons/socket.svg",
+            "/icons/nodejs.svg",
+            "/icons/express.svg",
+        ]
+    },
     {
         "img": "/images/blog_application.png", "website": "", "title": "Blog Application", "desc": "An application where users can post blogs and other users can see their posts and can do comments.", "github": "https://github.com/hasmuddinansari/blog_app", "tech": [
             "/icons/react.svg",
@@ -24,13 +32,14 @@ export default [
             "/icons/redux.svg"
         ]
     },
-    {
-        "img": "/images/car-rental.jpg", "website": "https://car-rental-system.netlify.com", "title": "Car Rent", "desc": "A web app where users can rent a car based on their location.", "github": "https://github.com/hasmuddinansari/car_rental", "tech": ["/icons/html.svg",
+
+{
+	"img":"/images/bookmymovies.png", "website": "https://hasmuddinansari.github.io/ticket_management/index.html", "title":"BookMyMovies", "desc":"An website where user can interact to buy an ticket for movies and can choose seat where they want.", "github":"https://github.com/hasmuddinansari/ticket_management", "tech": ["/icons/html.svg",
             "/icons/bootstrap.svg",
-            "/icons/react.svg",
-            "/icons/redux.svg"
+            "/icons/javascript.svg",
         ]
-    },
+},
+
     {
         "img": "/images/foodAdda.png", "website": "https://foodaddaa.netlify.com", "title": "Food Adda", "desc": "Find all kinds of foods and receipe with video tutorials", "github": "https://github.com/ayaanansari/foodAdda", "tech": ["/icons/html.svg",
             "/icons/bootstrap.svg",
@@ -44,12 +53,14 @@ export default [
             "/icons/redux.svg"
         ]
     },
+
 {
-	"img":"/images/bookmymovies.png", "website": "https://hasmuddinansari.github.io/ticket_management/index.html", "title":"BookMyMovies", "desc":"An website where user can interact to buy an ticket for movies and can choose seat where they want.", "github":"https://github.com/hasmuddinansari/ticket_management", "tech": ["/icons/html.svg",
+        "img": "/images/car-rental.jpg", "website": "https://car-rental-system.netlify.com", "title": "Car Rent", "desc": "A web app where users can rent a car based on their location.", "github": "https://github.com/hasmuddinansari/car_rental", "tech": ["/icons/html.svg",
             "/icons/bootstrap.svg",
-            "/icons/javascript.svg",
+            "/icons/react.svg",
+            "/icons/redux.svg"
         ]
-},
+    },
 {
         "img": "/images/blogify.png", "website": "https://blogifyy.netlify.app/", "title": "Blogifyy", "desc": "Blogify website is provide to user that they can create their blog and can do like on any blogs,", "github": "https://github.com/hasmuddinansari/blogify", "tech": ["/icons/html.svg",
             "/icons/bootstrap.svg",
